b086613d4869720ec3a9fb1aef0080e4182b46c4
github-username <72346436+Pashabn@users.noreply.github.com>
2022-06-22 20:38:20 +0300

    feat: changed the index.js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(n) {
+    const number = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+    const ten = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+    return n > 19
+        ? n < 10 ** 2
+            ? n % 10 >= 1
+                ? ten[Math.floor(n / 10)] + ' ' + number[n % 10]
+                : ten[n / 10]
+            : n < 10 ** 3
+                ? n % 10 ** 2 >= 1
+                    ? toReadable(Math.floor(n / 10 ** 2)) + ' hundred ' + toReadable(n % 10 ** 2)
+                    : toReadable(n / 10 ** 2) + ' hundred'
+                : 'very big number'
+        : number[n]
 }
